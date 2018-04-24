a60d8671e49c730cc875b2388540b1a5afb75ae9
Yamagn <syanekenpon@gmail.com>
2018-04-25 08:44:54 +0900

    Complete easy-chat

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var app = require('express')(); // appとしてexpressを初期化 httpserver使えるようにする
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket) {
+    console.log('a user connected'); // userがコネクトしたら表示
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg); // char maessage が入力されたらすべての人にメッセージを送信
+    });
+    socket.on('disconnect', function() {
+        console.log("user disconnected"); // userがディスコネクトしたら表示
+    });
+});
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
